efc623ad195e42c99aadae55d84e10bab1aa9882
Myazurite <longnvh2110@gmail.com Ceasar.248>
2021-11-02 09:04:57 +0700

    modify

--- src/medicine/Constant/Constant.java
+++ src/medicine/Constant/Constant.java
@@ -4,10 +4,10 @@ public class Constant {
     public static final String ROOT = System.getProperty("user.dir");
     public static final String MEDICIN_INFORMATION = System.getProperty("user.dir") + "\\RootFile";
     public static final String MEDICIN_IMAGE = System.getProperty("user.dir") + "\\Image";
-    
+
     public static void main(String[] args) {
-        String s1 = "viettuts %is a very good website";
-        String replaceString = s1.replace('%', '_');//thay thế tất cả ký tự 't' thành 'j'  
+        String s1 = " ";
+        String replaceString = s1.replace('%', '_');
         System.out.println(replaceString);
     }
 }
